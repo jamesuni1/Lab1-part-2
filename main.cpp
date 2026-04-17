033d0d7ccb32e7b50955144740227537d91e5c61
jamesuni1 <james2.stoves@live.uwe.ac.uk>
2026-04-17 17:14:26 +0100

    Complete part 2

--- main.cpp
+++ main.cpp
@@ -1,33 +1,33 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2019 ARM Limited
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #include "mbed.h"
-#include "arm_book_lib.h"
+
+
+// Blinking rate in milliseconds
+#define BLINKING_RATE     500ms
+
 
 int main()
 {
-    DigitalIn b1(BUTTON1);
-    DigitalIn d2(D2);
-    DigitalIn d3(D3);
-    DigitalIn d4(D4);
-    DigitalIn d5(D5);
-    DigitalIn d6(D6);
-    DigitalIn d7(D7);
+    // Initialise the digital pin LED1 as an output
     DigitalOut led1(LED1);
     DigitalOut led2(LED2);
     DigitalOut led3(LED3);
 
-    b1.mode(PullDown);
-    d2.mode(PullDown);
-    d3.mode(PullDown);
-    d4.mode(PullDown);
-    d5.mode(PullDown);
-    d6.mode(PullDown);
-    d7.mode(PullDown);
-    led1 = OFF;
-    led2 = OFF;
-    led3 = OFF;
+    int count =  0;
 
     while (true) {
-        led1 = b1 || d2 || d3;
-        led2 = b1 || d4 || d5;
-        led3 = b1 || d6 || d7;
+        led2 = !led2;
+        if (count % 2 == 0) {
+            led1 = !led1;
+        }
+        if (count % 4 == 0) {
+            led3 = !led3;
+        }
+        ThisThread::sleep_for(BLINKING_RATE);
+        count++;
     }
 }
